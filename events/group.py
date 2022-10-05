d52df6524c1200ddd1e4603332879da64f1cc2e9
hsn2 <912372447@qq.com>
2022-10-05 16:49:56 +0800

    取群聊设置接口

--- events/group.py
+++ events/group.py
@@ -258,3 +258,41 @@ class GetGroupMembersList:
                 return ReturnData(ReturnData.ERROR, 'you are not yet a member of this group')
         else:
             return msg
+
+
+class GetGroupSettings:
+    def __init__(self, server: HCatServer, req):
+        self.username: str
+        self.token: str
+        self.group_id: str
+        self.server: HCatServer = server
+        self.return_data = self._run(server, req)
+
+    def _run(self, server: HCatServer, request):
+        req_data = request_parse(request)
+        # 判断请求体是否为空
+        if 'username' not in req_data or 'token' not in req_data or 'group_id' not in req_data:
+            return ReturnData(ReturnData.ERROR, 'username token or group_id is missing')
+
+        # 获取请求参数
+        self.username = req_data['username']
+        self.token = req_data['token']
+        self.group_id = req_data['group_id']
+
+        # 验证用户名与token
+        auth_status, msg = server.authenticate_token(self.username, self.token)
+        if auth_status:
+            # 判断是否存在群聊
+            if not server.groups_db.exists(self.group_id):
+                return ReturnData(ReturnData.NULL, 'group not exists')
+
+            # 获取群租
+            group: Group = server.groups_db.get(self.group_id)
+
+            # 返回数据
+            if self.username in group.member_list:
+                return ReturnData(ReturnData.OK).add('data', group.group_settings)
+            else:
+                return ReturnData(ReturnData.ERROR, 'you are not yet a member of this group')
+        else:
+            return msg
