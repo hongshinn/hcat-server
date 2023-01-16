39534e40c89f30862f8b458faa6500b46a536630
hsn2 <912372447@qq.com>
2023-01-16 15:36:16 +0800

    fix:aes key

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from server import HCatServer
 
 if not os.path.exists(os.path.join(os.getcwd(), 'pri.key')):
     with open(os.path.join(os.getcwd(), 'pri.key'), 'w', encoding='utf8') as f:
-        f.write(util.get_random_token(1024))
+        f.write(util.get_random_token(256))
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s')
 logging.getLogger().addFilter(util.FlaskLoggerFilter())
